cad47f0b1f020e60407d20b8ad580cbe2cd64140
crflick88 <crflick88@gmail.com>
2016-03-18 15:45:56 -0700

    "port again"

--- server.js
+++ server.js
@@ -4,7 +4,7 @@
 // =============================================================================
 var mongoose   = require('mongoose');
 //mongoose.connect('mongodb://localhost:27017/movie'); // connect to our database
-mongoose.connect('mongodb://user:user@15909.mlab.com:15909/movie'); // connect to our database
+mongoose.connect('mongodb://user:user@ds015909.mlab.com:15909/movie'); // connect to our database
 
 var Movie = require('./app/models/movie');
 
